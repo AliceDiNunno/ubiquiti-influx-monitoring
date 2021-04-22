08c47095dc8b8847a852322a564db5e176702dce
Alice Di Nunno <alice@dinunno.fr>
2021-04-22 21:30:08 +0200

    [perf] Rx declared once instead of on every run

--- src/tools/hostname.go
+++ src/tools/hostname.go
@@ -2,11 +2,11 @@ package tools
 
 import "regexp"
 
+var validator = regexp.MustCompile(`^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`)
+
 func ValidateHostName(str string) bool {
-	var validator = regexp.MustCompile(`^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`)
 
 	matches := validator.MatchString(str)
 
-	validator = nil
 	return matches
 }
